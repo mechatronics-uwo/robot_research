4949d1ee555ccc975969ecb0336d8f382d98ca44
dannyloo <dannyloo2005@hotmail.com>
2015-03-29 17:09:53 -0400

    Ignore this

--- code/final/final.ino
+++ code/final/final.ino
@@ -70,14 +70,7 @@ unsigned long echo_time[2];//0 is front timer, 1 is back timer
 
 int light_value=0;
 int next_light_value=0;
-<<<<<<< HEAD
 int count=0;//counts the lights
-=======
-
-int count=0;//counts the lights
-
-
->>>>>>> origin/master
 unsigned int Left_Motor_Speed;
 unsigned int Right_Motor_Speed;
 unsigned int Right_Motor_Stop = 1500;
@@ -90,7 +83,7 @@ int Right_Motor_Offset = 30;
 
 unsigned long time_previous = 0; // Used for time functions, do not change
 unsigned long time_elapsed = 0; // Used for time functions, do not change
-boolean can_start_waiting = true; // Used for time functions, do not change
+boolean can_start_waiting = false; // Used for time functions, do not change
 
 
 
@@ -99,11 +92,7 @@ boolean can_start_waiting = true; // Used for time functions, do not change
 // NOTE: Stage 0 is reserved for debugging
 
 
-<<<<<<< HEAD
 unsigned int stage = 21;
-=======
-unsigned int stage = 0;
->>>>>>> origin/master
 
 
 // ******************************************************************
@@ -167,33 +156,16 @@ void loop() {
 
     case 0:
       // RESERVED FOR TESTING, PASTE CODE HERE AND SET STAGE = 0
-<<<<<<< HEAD
       moveForward(300);
       
-=======
-      smartMoveForwards();
-      if (hitWall()){
-        setNeutral();
-        stage = 30;
-      }
->>>>>>> origin/master
       break;
 
     case 1:
       // Case status: DONE by Daniel
       // Start case: Robot is in the middle of the room
-      moveForward(200);
-      if(hitWall()){
-        setNeutral();
-        moveBackDistance(500);
-        turnLeftAngle(87);
-        stage = 2;
-      }
-      else if(hitTable()){
-        setNeutral();
-        moveBackDistance(500);
-        turnLeftAngle(87);
-        stage = 3;
+      moveForward(350);
+      if(hitWall()){        
+        stage++;
       }
       break;
       // End case: Robot is parallel to the wall
@@ -202,14 +174,9 @@ void loop() {
       // Start case: Wall is in front of robot, robot is parallel to it
         moveBackDistance(300);
         turnLeftAngle(90);
-<<<<<<< HEAD
         
         stage++;
       // End case: Robot is hit the wall
-=======
-        stage = 3;
-      // End case: Table is in front of robot, robot is parallel to it
->>>>>>> origin/master
       break;
 
     case 3:
@@ -314,8 +281,7 @@ void loop() {
     // ==================== CASE 21-30 ====================
 
     case 21:
-      updateUltrasonics();
-      followWall();
+      moveForward(200);
       countLight();
       
       if(count==1)
@@ -371,28 +337,21 @@ void loop() {
 // Ping ultrasonic
 // Send the Ultrasonic Range Finder a 10 microsecond pulse per tech spec
 
-float frontPing() {
+int frontPing() {
   //Front ultrasonic
   digitalWrite(ULTRASONIC_IN_PIN_FRONT, HIGH);
   delayMicroseconds(10); //The 10 microsecond pause where the pulse in "high"
   digitalWrite(ULTRASONIC_IN_PIN_FRONT, LOW);
 
-<<<<<<< HEAD
   unsigned long ping_time1 = pulseIn(ULTRASONIC_OUT_PIN_FRONT, HIGH, 10000);
 
   // Serial.print("front: ");
   // Serial.println(ping_time1); //divide time by 58 to get
-=======
-  float ping_time = pulseIn(ULTRASONIC_OUT_PIN_FRONT, HIGH, 10000);
-
-  Serial.print("cm: ");
-  Serial.println(ping_time); //divide time by 58 to get
->>>>>>> origin/master
 
   return ping_time1;
 }
 
-float backPing() {
+int backPing() {
   //Back ultrasonic
   digitalWrite(ULTRASONIC_IN_PIN_BACK, HIGH);
   delayMicroseconds(10); //The 10 microsecond pause where the pulse in "high"
@@ -402,19 +361,9 @@ float backPing() {
   // time that it takes from when the Pin goes HIGH until it goes LOW
   unsigned long ping_time2 = pulseIn(ULTRASONIC_OUT_PIN_BACK, HIGH, 10000);
 
-<<<<<<< HEAD
   // Serial.print("back: ");
   // Serial.println(ping_time2); //divide time by 58 to get distance in cm
   return ping_time2;
-=======
-  float ping_time = pulseIn(ULTRASONIC_OUT_PIN_BACK, HIGH, 10000);
-
-
-  Serial.print("cm: ");
-  Serial.println(ping_time); //divide time by 58 to get distance in cm
-  return ping_time;
-
->>>>>>> origin/master
 }
 
 
@@ -446,12 +395,13 @@ boolean hitTable() {
 boolean hitWall() {
   int bottom_lever = digitalRead(FRONT_BOTTOM_LEVER_SWITCH_PIN);
   int top_lever = digitalRead(FRONT_TOP_LEVER_SWITCH_PIN);
-  if (top_lever == LOW){
+  if ((top_lever == LOW) && (bottom_lever == LOW)){
     Serial.println("Wall");
     return true;
 
   }
   else{
+    Serial.println("Nothing");
     return false;
   }
 
@@ -470,11 +420,7 @@ void countLight()
       next_light_value = analogRead(right_light_sensor);     
       if((next_light_value < light_value) && (next_light_value < 50))
       {        
-<<<<<<< HEAD
         moveFowardDistance(1100);        
-=======
-        moveFowardDistance(1000);        
->>>>>>> origin/master
         count++;
                
       }
@@ -515,50 +461,6 @@ void moveBackwardsFixed(){
   implementMotorSpeed();
 }
 
-void smartMoveForwards(){
-  // Keep between 800 and 450 for ping
-  startWaiting();
-  float front_ping;
-  float back_ping;
-  if (waitMilliSecond(1000)){
-    front_ping = frontPing();
-    delay(10);
-    back_ping = backPing();
-    if ((back_ping - front_ping) > 600){
-      reAlign(front_ping);
-      Serial.println("Realigning");
-    }
-    else if (front_ping > 800){
-      veerRight(200, 100);
-      Serial.println("Too far, need to veer right");
-    }
-    else if (front_ping < 450){
-      setNeutral();
-      turnLeftAngle(10);
-      veerLeft(200, 100);
-      Serial.println("Too close, need to veer left");
-    }
-    else {
-      moveForward(200);
-      Serial.println("Everything's perfect");
-    }
-  }
-}
-
-void reAlign(float ping_value){
-    float front_ping = ping_value;
-    if (front_ping < 300){
-      setNeutral();
-      turnLeftAngle(25);
-      moveForward(200);
-    }
-    else{
-      moveForward(200);
-      delay(1000);
-      setNeutral();
-      turnLeftAngle(20);
-    }
-}
 
 void moveForward(long speedFactor)
 {
@@ -705,17 +607,16 @@ void brake() {
 // Call startWaiting first, and then waitMilliSecond
 void startWaiting() {
   if (can_start_waiting) {
-    Serial.println("Started waiting");
+
     time_previous = millis();
-    can_start_waiting = false; //Toggle false to stop it from resetting the timer until it's done waiting
   }
 }
 
-boolean waitMilliSecond(unsigned long interval) {
+boolean waitMilliSecond(unsigned int interval) {
 
   time_elapsed = millis();
   if ((time_elapsed - time_previous) > interval) {
-    can_start_waiting = true;
+    can_start_waiting = false;
     return true; // Done waiting!
   }
   else {
